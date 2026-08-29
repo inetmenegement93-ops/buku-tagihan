eb009760da0517c19ae92e2d09edcae801879ffd
inetmenegement93-ops <inet.menegement93@gmail.com>
2026-08-29 11:31:01 +0700

    Add files via upload

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,11 @@
-// Config project Firebase Anda (buku-tagihan-1ba54). Kalau suatu saat ganti project,
+// Config project Firebase Anda (buku-tagihan-439e0). Kalau suatu saat ganti project,
 // ambil ulang dari Firebase Console -> Project settings -> General -> Your apps.
 export const firebaseConfig = {
-  apiKey: "AIzaSyB7qv17rf1rcru_kdvnKCeUCqeBIF9QSCk",
-  authDomain: "buku-tagihan-1ba54.firebaseapp.com",
-  projectId: "buku-tagihan-1ba54",
-  storageBucket: "buku-tagihan-1ba54.firebasestorage.app",
-  messagingSenderId: "58609477439",
-  appId: "1:58609477439:web:8cee8f14c46cd4ba1a158a",
-  measurementId: "G-9JLDWYJPQ4",
+  apiKey: "AIzaSyCVoQJ7qjlNZYZrwvLg-l1q3R49jfj5Tl0",
+  authDomain: "buku-tagihan-439e0.firebaseapp.com",
+  projectId: "buku-tagihan-439e0",
+  storageBucket: "buku-tagihan-439e0.firebasestorage.app",
+  messagingSenderId: "971774017649",
+  appId: "1:971774017649:web:3fef855dd9fd095f675bd0",
+  measurementId: "G-K4N7W57220",
 };
